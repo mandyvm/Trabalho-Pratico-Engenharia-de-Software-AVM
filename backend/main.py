d563429e01a202a2127c762e3247118f590db0be
unknown <madsprivd@gmail.com>
2026-06-07 23:55:39 -0300

    atualização para o tp5

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="API do Dashboard Epidemiológico")
+app = FastAPI(title="API do Dashboard Epidemiológico ")
 
 app.add_middleware(
     CORSMiddleware,
